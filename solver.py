8f5e734b7d483c285f6bc7c95658bfa212efe42a
Justin Dugger <jldugger@gmail.com>
2021-06-06 18:45:18 -0700

    add demo puzzle

--- solver.py
+++ solver.py
@@ -10,8 +10,7 @@ def parse_opts():
     parser.add_argument('filename', metavar='filename', type=str)
     return parser.parse_args()
 
-def parse_file(filename):
-    puzzle = []
+def parse_file(filename, puzzle=[]):
     with open(filename, "r") as f:
         for line in f:
             puzzle_line, puzzle_val = [], 0
@@ -34,8 +33,7 @@ def puzzle_print(puzzle):
             print(sym_map[puzzle[x][y]], end=None if y == 4 else '')
     print()
 
-def convert(puzzle):
-    A = []
+def convert(puzzle, A = []):
     for x in range(5): # for every line
         count = {"C":0, "X":0, "P":0, "S":0, "R":0}
         for y in range(5):
@@ -50,19 +48,15 @@ def convert(puzzle):
             count[c] += 1
         A.append(list(count.values()))
 
-    b = [puzzle[i][5] for i in range(5)] + [puzzle[5][i] for i in range(5)]
-
-    return (A, b)
+    return (A, [puzzle[i][5] for i in range(5)] + [puzzle[5][i] for i in range(5)])
 
 def solve(A, b):
-    A = np.array(A)
-    b = np.array(b)
+    A, b = np.array(A), np.array(b)
     z = np.linalg.lstsq(A, b, rcond=None)[0]
     return z
 
 def write_solution(z):
-    out = zip(["circle", "cross","pentagon", "square", "star"], z)
-    for (sym, val) in out:
+    for (sym, val) in zip(["circle", "cross","pentagon", "square", "star"], z):
         print(f"{sym}: {round(val)}")
 
 def main():
