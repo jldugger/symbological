84690dd200ddea6214fa762d564f0d3dbc4392d2
Justin Dugger <jldugger@gmail.com>
2021-06-06 17:49:24 -0700

    refactor to generic solver

--- solver.py
+++ solver.py
@@ -4,32 +4,87 @@ import numpy as np
 
 symbols = ["circle", "cross","pentagon", "square", "star"]
 
+sym_map = {
+    "C":"● ",
+    "X":"✚ ",
+    "P":"⬟ ",
+    "S":"■ ",
+    "R":"★ "
+}
 
-def puzzle_one_twelve():
-    # circle, cross, pentagon, square, star
-    A = np.array([
-        [3, 1, 0, 1, 0],
-        [1, 0, 0, 2, 2],
-        [2, 1, 0, 1, 1],
-        [1, 0, 1, 1, 2],
-        [2, 2, 1, 0, 0],
-        [1, 1, 0, 2, 1],
-        [2, 1, 1, 0, 1],
-        [2, 1, 1, 1, 0],
-        [3, 0, 0, 1, 1],
-        [1, 1, 0, 1, 2],
-    ])
-    b = np.array([20, 24, 24, 20, 23, 27, 20, 19, 17, 28])
+DEBUG = False
 
-    # A * z = b
+def debug(s, end=None):
+    if DEBUG:
+        print(s, end=end)
+
+def parse():
+    puzzle = []
+    with open('one_twelve.txt', "r") as f:
+        for line in f:
+            puzzle_line= []
+            puzzle_val = 0
+            for c in line:
+                if c == " ":
+                    if puzzle_val != 0:
+                        puzzle_line.append(puzzle_val)
+                    puzzle_val = 0
+                if c in sym_map:
+                    debug(sym_map[c], end='')
+                    puzzle_line.append(c)
+                if c in "0123456789":
+                    puzzle_val = puzzle_val * 10 + int(c)
+            debug(f" {puzzle_val}")
+            puzzle_line.append(puzzle_val)
+            puzzle.append(puzzle_line)
+    debug(None)
+    return puzzle
+
+def convert(puzzle):
+    A, b = [], []
+    for x in range(5): # for every line
+        count = {"C":0, "X":0, "P":0, "S":0, "R":0}
+        for y in range(5):
+            c = puzzle[x][y]
+            count[c] += 1
+        A.append(list(count.values()))
+        debug(A[-1])
+
+    for y in range(5): # for every row
+        count = {"C":0, "X":0, "P":0, "S":0, "R":0}
+        for x in range(5):
+            c = puzzle[x][y]
+            count[c] += 1
+        A.append(list(count.values()))
+
+    for i in range(5):
+        val = puzzle[i][5]
+        debug(val)
+        b.append(val)
+
+    for i in range(5):
+        val= puzzle[5][i]
+        debug(val)
+        b.append(val)
+    return (A, b)
+
+def solve(A, b):
+    A = np.array(A)
+    b = np.array(b)
     z = np.linalg.lstsq(A,b, rcond=None)[0]
+    return z
+
+def write_solution(z):
     out = zip(symbols,z)
     for (sym, val) in out:
         print(f"{sym}: {round(val)}")
 
-
 def main():
-    puzzle_one_twelve()
+    puzzle = parse()
+    debug(puzzle)
+    (A, b)=convert(puzzle)
+    z = solve(A, b)
+    write_solution(z)
 
 if __name__ == "__main__":
     main()
