eb54f9456d005f182e66d9927b4299b67664b49f
Justin Dugger <jldugger@gmail.com>
2021-06-06 18:31:40 -0700

    list comprehension golf

--- solver.py
+++ solver.py
@@ -36,7 +36,7 @@ def puzzle_print(puzzle):
     print()
 
 def convert(puzzle):
-    A, b = [], []
+    A = []
     for x in range(5): # for every line
         count = {"C":0, "X":0, "P":0, "S":0, "R":0}
         for y in range(5):
@@ -51,13 +51,8 @@ def convert(puzzle):
             count[c] += 1
         A.append(list(count.values()))
 
-    for i in range(5):
-        val = puzzle[i][5]
-        b.append(val)
+    b = [puzzle[i][5] for i in range(5)] + [puzzle[5][i] for i in range(5)]
 
-    for i in range(5):
-        val= puzzle[5][i]
-        b.append(val)
     return (A, b)
 
 def solve(A, b):
